e7d0e76993b0222ae151421586ffbb0f3de46abe
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-03-17 10:43:35 -0500

    Improve crafting trimmed / dyed Pots

--- src/main/java/com/teamabnormals/clayworks/common/item/crafting/DecoratedPotColoring.java
+++ src/main/java/com/teamabnormals/clayworks/common/item/crafting/DecoratedPotColoring.java
@@ -1,11 +1,19 @@
 package com.teamabnormals.clayworks.common.item.crafting;
 
+import com.teamabnormals.clayworks.common.DecoratedPotTrim;
+import com.teamabnormals.clayworks.common.DecoratedPotTrimPattern;
 import com.teamabnormals.clayworks.core.registry.ClayworksBlocks;
+import com.teamabnormals.clayworks.core.registry.ClayworksDataComponents;
 import com.teamabnormals.clayworks.core.registry.ClayworksRecipes.ClayworksRecipeSerializers;
+import com.teamabnormals.clayworks.core.registry.ClayworksRegistries;
+import net.minecraft.core.Holder.Reference;
 import net.minecraft.core.HolderLookup.Provider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.armortrim.TrimMaterial;
+import net.minecraft.world.item.armortrim.TrimMaterials;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
 import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.CustomRecipe;
@@ -15,6 +23,8 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DecoratedPotBlock;
 import net.neoforged.neoforge.common.Tags;
 
+import java.util.Optional;
+
 public class DecoratedPotColoring extends CustomRecipe {
 
 	public DecoratedPotColoring(CraftingBookCategory category) {
@@ -23,50 +33,88 @@ public class DecoratedPotColoring extends CustomRecipe {
 
 	@Override
 	public boolean matches(CraftingInput input, Level level) {
-		int i = 0;
-		int j = 0;
+		int ptCount = 0;
+		int dyeCount = 0;
+		int trimCount = 0;
 
-		for (int k = 0; k < input.size(); ++k) {
-			ItemStack stack = input.getItem(k);
+		ItemStack pot = ItemStack.EMPTY;
+		for (int slot = 0; slot < input.size(); ++slot) {
+			ItemStack stack = input.getItem(slot);
 			if (!stack.isEmpty()) {
 				if (Block.byItem(stack.getItem()) instanceof DecoratedPotBlock) {
-					++i;
-				} else {
-					if (!stack.is(Tags.Items.DYES)) {
+					pot = stack.copy();
+					++ptCount;
+				}
+			}
+		}
+
+		if (ptCount != 1 || pot.isEmpty()) {
+			return false;
+		}
+
+		for (int slot = 0; slot < input.size(); ++slot) {
+			ItemStack stack = input.getItem(slot);
+			if (!stack.isEmpty()) {
+				if (stack.is(Tags.Items.DYES)) {
+					++dyeCount;
+					if (DyeColor.getColor(stack) == ClayworksBlocks.getDyeColorFromPot(Block.byItem(pot.getItem()))) {
 						return false;
 					}
-
-					++j;
+				} else if (stack.is(ItemTags.TRIM_MATERIALS)) {
+					++trimCount;
+					if (pot.getComponents().get(ClayworksDataComponents.POT_TRIM.get()) != null) {
+						return false;
+					}
+				} else if (!(Block.byItem(stack.getItem()) instanceof DecoratedPotBlock)) {
+					return false;
 				}
 
-				if (j > 1 || i > 1) {
+				if (dyeCount > 1 || trimCount > 1) {
 					return false;
 				}
 			}
 		}
 
-		return i == 1 && j == 1;
+		return dyeCount == 1 || trimCount == 1;
 	}
 
 	@Override
 	public ItemStack assemble(CraftingInput input, Provider provider) {
-		ItemStack stack = ItemStack.EMPTY;
+		ItemStack output = ItemStack.EMPTY;
 		DyeColor color = DyeColor.WHITE;
 
-		for (int i = 0; i < input.size(); ++i) {
-			ItemStack itemstack1 = input.getItem(i);
-			if (!itemstack1.isEmpty()) {
-				Item item = itemstack1.getItem();
-				if (Block.byItem(item) instanceof DecoratedPotBlock) {
-					stack = itemstack1;
-				} else {
-					DyeColor tmp = DyeColor.getColor(itemstack1);
-					if (tmp != null) color = tmp;
+		for (int slot = 0; slot < input.size(); ++slot) {
+			ItemStack stack = input.getItem(slot);
+			if (!stack.isEmpty()) {
+				Item item = stack.getItem();
+				if (Block.byItem(item) instanceof DecoratedPotBlock block) {
+					output = stack.copy();
+					output.setCount(1);
+					color = ClayworksBlocks.getDyeColorFromPot(block);
+					break;
 				}
 			}
 		}
 
-		return stack.transmuteCopy(ClayworksBlocks.getPotFromDyeColor(color));
+		for (int slot = 0; slot < input.size(); ++slot) {
+			ItemStack stack = input.getItem(slot);
+			if (!stack.isEmpty()) {
+				if (stack.is(Tags.Items.DYES)) {
+					DyeColor tmp = DyeColor.getColor(stack);
+					if (tmp != null) {
+						color = tmp;
+					}
+				} else if (stack.is(ItemTags.TRIM_MATERIALS)) {
+					Optional<Reference<TrimMaterial>> material = TrimMaterials.getFromIngredient(provider, stack);
+					Optional<Reference<DecoratedPotTrimPattern>> pattern = provider.lookupOrThrow(ClayworksRegistries.DECORATED_POT_TRIM_PATTERN).get(DecoratedPotTrimPattern.BASE);
+					if (material.isPresent() && pattern.isPresent()) {
+						output.set(ClayworksDataComponents.POT_TRIM.get(), new DecoratedPotTrim(material.get(), pattern.get(), true));
+					}
+				}
+			}
+		}
+
+		return output.transmuteCopy(ClayworksBlocks.getPotFromDyeColor(color));
 	}
 
 	@Override
